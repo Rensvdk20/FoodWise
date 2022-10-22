35739343938657afa4acad2fa6d08f63a59c0466
Rensvdk20 <rensvdk@gmail.com>
2022-10-22 14:20:12 +0200

    Added DomainServices

---
+++ DomainServices/Repos/ICanteenEmployeeRepo.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainServices.Repos
+{
+    public interface ICanteenEmployeeRepo
+    {
+        IQueryable<CanteenEmployee> GetAllCanteenEmployees();
+        CanteenEmployee GetCanteenEmployeeById(int id);
+    }
+}
---
+++ DomainServices/Repos/IPackageRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace DomainServices.Repos
+{
+    public interface IPackageRepo
+    {
+        IQueryable<Package> GetAllPackages();
+        Package GetPackageById(int id);
+
+        Task AddPackage(Package newPackage);
+        Task EditPackage(Package editedPackage);
+        Task DeletePackageById(int id);
+    }
+}
